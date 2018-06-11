21277dadd90a7c23327368f3165ca8a7d2033063
Erik Kvale <eirikval@gmail.com>
2018-06-11 14:05:26 -0500

    Customization settings for admin

--- core/admin.py
+++ core/admin.py
@@ -5,3 +5,6 @@ from core.models import (
 
 # Register your models here.
 admin.site.register(Movie)
+admin.site.site_header = 'myMDB Admin'
+admin.site.site_title = 'myMDB Admin Portal'
+admin.site.index_title = 'Welcome to the myMDB Admin Portal'
