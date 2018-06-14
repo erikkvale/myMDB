be82a9c68b4a74d7455fce150a9d0309fbfa8f48
Erik Kvale <eirikval@gmail.com>
2018-06-14 11:24:17 -0500

    Updated MovieDetail view by overriding and customizing get_context_data method

--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import (
     ListView,
     DetailView,
 )
-from core.models import Movie, Person
+from core.models import Movie, Person, Vote
 
 
 class MovieList(ListView):
@@ -13,6 +14,33 @@ class MovieList(ListView):
 
 class MovieDetail(DetailView):
     queryset = Movie.objects.all_with_related_persons()
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            vote = Vote.objects.get_vote_or_unsaved_blank_vote(
+                movie=self.object,
+                user=self.request.user
+            )
+            if vote.id:
+                vote_form_url = reverse(
+                    'core:UpdateVote',
+                    kwargs={
+                        'movie_id': vote.movie.id,
+                        'pk': vote.id
+                    }
+                )
+            else:
+                vote_form_url = reverse(
+                    'core:CreateVote',
+                    kwargs={
+                        'movie_id': self.object.id
+                    }
+                )
+            vote_form = VoteForm(instance=vote)
+            ctx['vote_form'] = vote_form
+            ctx['vote_form_url'] = vote_form_url
+        return ctx
 
 
 class PersonDetail(DetailView):
