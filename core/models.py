aef950db7992f79b877f441888968309dac47858
Erik Kvale <eirikval@gmail.com>
2018-06-14 11:05:21 -0500

    Added custom Vote model manager

--- core/models.py
+++ core/models.py
@@ -24,6 +24,20 @@ class PersonManager(models.Manager):
             'role_set__movie'
         )
 
+class VoteManager(models.Manager):
+
+    def get_vote_or_unsaved_blank_vote(self, movie, user):
+        try:
+            return Vote.objects.get(
+                movie=movie,
+                user=user
+            )
+        except Vote.DoesNotExist:
+            return Vote(
+                movie=movie,
+                user=user
+            )
+
 
 #==========================================================
 # Models
