6283976ca226202e661f4e3ce611a348f9f114f9
Erik Kvale <eirikval@gmail.com>
2018-06-16 11:29:02 -0500

    Added MovieImageUpload view

--- core/views.py
+++ core/views.py
@@ -55,6 +55,32 @@ class MovieDetail(DetailView):
         return ctx
 
 
+class MovieImageUpload(LoginRequiredMixin, CreateView):
+    form_class = MovieImageForm
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['user'] = self.request.user.id
+        initial['movie'] = self.kwargs['movie_id']
+        return initial
+
+    def render_to_response(self, context, **response_kwargs):
+        movie_id = self.kwargs['movie_id']
+        movie_detail_url = reverse(
+            'core:MovieDetail',
+            kwargs={'pk': movie_id}
+        )
+        return redirect(to=movie_detail_url)
+
+    def get_success_url(self):
+        movie_id = self.kwargs['movie_id']
+        movie_detail_url = reverse(
+            'core:MovieDetail',
+            kwargs={'pk': movie_id}
+        )
+        return movie_detail_url
+
+
 class PersonDetail(DetailView):
     queryset = Person.objects.all_with_prefetch_movies()
 
