fe1b41a4a4eab6abc41f6ef33dd8d7a68e2b0ce2
Erik Kvale <eirikval@gmail.com>
2018-06-22 15:07:45 -0500

    Working on dev settings

---
+++ myMDB/settings/dev_settings.py
@@ -0,0 +1,29 @@
+from .base_settings import *
+
+DEBUG = True
+SECRET_KEY = 'some secret'
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myMDB',
+        'USER': 'postgres',
+        'PASSWORD': 'Gunnar14',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'default-locmemcache',
+        'TIMEOUT': 5
+    }
+}
+
+MEDIA_ROOT = os.path.join(BASE_DIR, '../../../../media_root')
