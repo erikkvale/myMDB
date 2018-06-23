c5db1c82e3a4e62b484dd94b3693cabbebe51184
Erik Kvale <eirikval@gmail.com>
2018-06-22 22:23:14 -0500

    Added file upload settings

--- myMDB/settings/production_settings.py
+++ myMDB/settings/production_settings.py
@@ -29,3 +29,9 @@ CACHES = {
         'TIMEOUT': int(os.getenv('DJANGO_CACHE_TIMEOUT'),)
     }
 }
+
+# File Uploads
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY_ID')
+AWS_STORAGE_BUCKET_NAME = os.getenv('DJANGO_UPLOAD_S3_BUCKET')
